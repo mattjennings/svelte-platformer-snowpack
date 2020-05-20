737d4d16cb13689aec1b1bf14e20cf601a2fb28f
Matt Jennings <mattjennings44@gmail.com>
2020-05-20 12:18:15 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,125 @@
+<script>
+  import Phaser from "phaser";
+  import {
+    Game,
+    Scene,
+    Text,
+    Camera,
+    Tilemap,
+    TileLayer,
+    ObjectLayer,
+    Spawner
+  } from "svelte-phaser";
+  import fragment from "svelte-fragment";
+  import Player from "./Player.svelte";
+
+  let game;
+
+  $: window.game = game;
+
+  function preload(scene) {
+    scene.load.tilemapTiledJSON("tilemaps/castle", "assets/tilemap.json");
+    scene.load.image("tilesets/castle", "assets/castle-tileset.png");
+    scene.load.spritesheet("textures/player", "assets/adventurer.png", {
+      frameWidth: 50,
+      frameHeight: 37
+    });
+  }
+
+  function create(scene) {
+    scene.anims.create({
+      key: "anims/player/idle",
+      frames: scene.anims.generateFrameNumbers("textures/player", {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 6,
+      repeat: -1
+    });
+    scene.anims.create({
+      key: "anims/player/run",
+      frames: scene.anims.generateFrameNumbers("textures/player", {
+        start: 8,
+        end: 13
+      }),
+      frameRate: 8,
+      repeat: -1
+    });
+    scene.anims.create({
+      key: "anims/player/jump",
+      frames: scene.anims.generateFrameNumbers("textures/player", {
+        start: 14,
+        end: 21
+      }),
+      frameRate: 24
+    });
+    scene.anims.create({
+      key: "anims/player/fall",
+      frames: scene.anims.generateFrameNumbers("textures/player", {
+        start: 22,
+        end: 23
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+  }
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<Game
+  bind:instance={game}
+  width={256}
+  height={224}
+  physics={{ default: 'arcade', arcade: { gravity: { y: 800 } } }}
+  render={{ pixelArt: true }}
+  scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}>
+  <Scene
+    key="main"
+    physics={{ arcade: { width: 512, height: 256 } }}
+    {preload}
+    {create}>
+    <template use:fragment slot="loading" let:progress>
+      <Text
+        x={100}
+        y={100}
+        text={`Loading... ${progress * 100}%`}
+        fontSize={8}
+        color="white" />
+    </template>
+
+    <Tilemap
+      key="tilemaps/castle"
+      tilesets={[{ key: 'tilesets/castle', name: 'castle-tileset' }]}>
+      <Spawner>
+
+        <TileLayer
+          id="ground"
+          name="ground"
+          tilesets={['castle-tileset']}
+          collisionTilesByProperty={{ collision: true }} />
+        <TileLayer id="bg" tilesets={['castle-tileset']} />
+      </Spawner>
+    </Tilemap>
+
+    <Camera
+      x={0}
+      y={0}
+      width={256}
+      height={224}
+      follow="player"
+      roundPixels
+      bounds={{ x: 0, y: 0, width: 512, height: 256 }}>
+
+      <Player x={100} y={168} />
+
+    </Camera>
+  </Scene>
+</Game>
---
+++ src/Player.svelte
@@ -0,0 +1,92 @@
+<script>
+  import Phaser from "phaser";
+  import {
+    Sprite,
+    ArcadePhysics,
+    getScene,
+    onGameEvent,
+    ArcadeCollider
+  } from "svelte-phaser";
+
+  export let x;
+  export let y;
+
+  let flipX = false;
+  let velocityX = 0;
+  let velocityY = 0;
+  let animation = "idle";
+
+  let instance;
+
+  const scene = getScene();
+  const X_SPEED = 100;
+  const keys = {
+    left: scene.input.keyboard.addKey("left"),
+    right: scene.input.keyboard.addKey("right"),
+    up: scene.input.keyboard.addKey("up"),
+    jump: scene.input.keyboard.addKey("SPACE")
+  };
+
+  // handle key inputs
+  onGameEvent("step", () => {
+    if (
+      (!keys.left.isDown && !keys.right.isDown) ||
+      (keys.left.isDown && keys.right.isDown)
+    ) {
+      velocityX = 0;
+    } else if (keys.left.isDown) {
+      velocityX = -X_SPEED;
+      flipX = true;
+    } else if (keys.right.isDown) {
+      flipX = false;
+      velocityX = X_SPEED;
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(keys.jump) && instance.body.onFloor()) {
+      velocityY = -200;
+      animation = "jump";
+    } else if (Phaser.Input.Keyboard.JustUp(keys.jump) && velocityY < 0) {
+      velocityY = 1;
+    }
+  });
+
+  $: if (velocityY === 0) {
+    if (velocityX !== 0) {
+      animation = "run";
+    } else {
+      animation = "idle";
+    }
+  }
+
+  $: if (velocityY < 0 && animation !== "fall") {
+    animation = "jump";
+  } else if (velocityY > 0) {
+    animation = "fall";
+  }
+
+  function onAnimationComplete({ detail }) {
+    if (detail.animation.key === "anims/player/jump") {
+      animation = "fall";
+    }
+  }
+</script>
+
+<Sprite
+  name="player"
+  bind:instance
+  bind:x
+  bind:y
+  animation={`anims/player/${animation}`}
+  on:animationcomplete={onAnimationComplete}
+  {flipX}>
+  <ArcadePhysics
+    collideWorldBounds
+    bind:velocityX
+    bind:velocityY
+    width={16}
+    height={32}
+    offsetX={16}
+    offsetY={4} />
+  <ArcadeCollider with="ground" />
+
+</Sprite>
